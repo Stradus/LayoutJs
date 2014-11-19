7f9302aa9c2977b9a611d89f57429c1979e9163b
OQO <oqo@stradus.com>
2014-11-19 15:55:06 +0200

    Added basic connecting (binding) of properties
    Basic binding between elements and POJOs (Plain Old Javascript Objects)
    also known as regular Javascript objects.
    
    We still do not dispose of bindings etc. so we now leak even more
    memory. But we are confident that we can increase the leakage even more
    in further commits!

--- src/core/properties.js
+++ src/core/properties.js
@@ -3,13 +3,126 @@ var Layout;
 (function (Layout) {
     var recordAccess = false;
 
+    var activeBindingProperty = undefined;
+    Layout.connectWithProperty = function (element, elementPropertyName, object, objectPropertyName) {
+        var elementProperty = getProperty(element, elementPropertyName);
+        if (!elementProperty) {
+            throw "Element has no property: " + elementPropertyName;
+        }
+        if (object.hasOwnProperty(objectPropertyName)) {
+            var property = getProperty(object, objectPropertyName);
+            if (!property) {
+                var desc = Object.getOwnPropertyDescriptor(object, objectPropertyName);
+                if (!desc) {
+                    throw "Can not bind to non-existing property: " + objectPropertyName;
+                }
+                if (desc.configurable === false) {
+                    throw "Can not create two way binding. This property cannot be configured: " + objectPropertyName;
+                }
+                var property = {
+                    element: object,
+                    name: objectPropertyName,
+                    isForeign: true,
+                    value: object[objectPropertyName],
+                    valueSet: true,
+                    partners: new Set(),
+                    subscribers: new Set(),
+                    originalGet: desc.get,
+                    originalSet: desc.set
+                }
+                var o = {
+                    get: function () {
+                        if (recordAccess) {
+                            accessedForRead(property);
+                        }
+                        if (property.originalGet) {
+                            // Call get function, since it might calculate values only on demand
+                            // and not use set at all,this would however probably cause problems
+                            // to automatic two-way binding
+                            property.value = property.originalGet();
+                        }
+                        return property.value;
+                    },
+                    set: function (v) {
+                        if (recordAccess) {
+                            accessedForWrite(property);
+                        }
+                        var originalValue = property.value;
+                        if (property.originalSet) {
+                            property.originalSet(v);
+                            if (property.originalGet) {
+                                property.value = property.originalGet();
+                            } else {
+                                property.value = v;
+                            }
+                        } else {
+                            property.value = v;
+                        }
+
+                        if (originalValue !== property.value) {
+                            handlePartners(property);
+                            handleSubscribers(property);
+                        } 
+                    },
+                    configurable: true
+                }
+                Object.defineProperty(object, objectPropertyName, o);
+                console.log("New property wrapper created for: " + objectPropertyName);
+            }
+        } else {
+            throw "Non-existing property name on object: " + objectPropertyName;
+        }
+        property.partners.add(elementProperty);
+        elementProperty.partners.add(property);
+        elementProperty.element[elementProperty.name] = property.element[property.name];
+    };
+
+    var propertyMap = new WeakMap();
+    var addElementPropertyToMap = function (property) {
+        addObjectPropertyToMap(property.element, property.name, property);
+    };
+    var addObjectPropertyToMap = function (object, name, property) {
+        var v = propertyMap.get(object);
+        if (!v) {
+            v = new Map();
+            propertyMap.set(object, v);
+        }
+        v.set(name, property);
+    }
+
+    var getProperty = function (element, propertyName) {
+        var map = propertyMap.get(element);
+        if (!map) {
+            return map;
+        }
+        return map.get(propertyName);
+    }
+    var handlePartners = function (property) {
+        activeBindingProperty = activeBindingProperty || property;
+        try {
+            property.partners.forEach(function (p) {
+                if (p !== activeBindingProperty) {
+                    p.element[p.name] = property.value;
+                }
+            });
+        }
+        finally {
+            activeBindingProperty = undefined;
+        }
+    }
+
+    var handleSubscribers = function (property) {
+        property.subscribers.forEach(function (p) {
+            p.updateValue();
+        });
+    };
 
     Layout.addProperty = function (element, name, options) {
         if (!options) {
             options = { get: true, set: true };
         }
-        if(typeof options === 'function'){
-            options = { get: true, set: true, onChange:options };
+        if (typeof options === 'function') {
+            options = { get: true, set: true, onChange: options };
         }
         var property = {
             element: element,
@@ -18,6 +131,7 @@ var Layout;
             valueSet: false,
             onChange: options.onChange,
             subscribers: new Set(),
+            partners: new Set(),
             needsMeasure: options.needsMeasure,
             needsArrange: options.needsArrange,
             needsRender: options.needsRender
@@ -56,13 +170,14 @@ var Layout;
                 if (property.needsRender) {
                     property.element.needsRender = true;
                 }
-                property.subscribers.forEach(function (p) {
-                    p.updateValue();
-                });
-            }
-        };
+                handlePartners(property);
+                handleSubscribers(property);
+                
+            };
+        }
         Object.defineProperty(element, name, o);
         o.set(options.default);
+        addElementPropertyToMap(property);
         return property;
     };
 
@@ -101,9 +216,7 @@ var Layout;
             compute: compute,
             updateValue: function () {
                 property.value = evaluateProperty(property);
-                property.subscribers.forEach(function (p) {
-                    p.updateValue();
-                });
+                handleSubscribers(property);
             }
         };
         var o = {};
@@ -115,6 +228,7 @@ var Layout;
         };
         Object.defineProperty(element, name, o);
         property.updateValue();
+        addElementPropertyToMap(property);
         return property;
     };
 
